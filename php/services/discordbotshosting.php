c6742fbc2211772ed33f7fcd3b48912805a38a33
lz20r <zhiyaonaiara@gmail.com>
2024-06-01 16:32:04 +0200

    chore: Update discordbotshosting.php page layout and styling

---
+++ php/services/discordbotshosting.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>https://cinammon.es/services/discordbot</title>
+
+    <link rel="stylesheet" href="/styling/services/styledb.css">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
+
+</head>
+
+<body>
+
+    <header class="header">
+        <a href="/php/index.php#home" class="logo">cinammon.es</a>
+
+        <i class="bx bx-menu" id="menu-icon"></i>
+
+        <nav class="navbar">
+            <a href="/php/index.php#home" class="active">Inicio</a>
+            <a href="/php/index.php#about" class="active">Nosotros</a>
+            <a href="/php/index.php#services" class="active">Servicios</a>
+            <a href="/php/index.php#billing" class="active">Facturación</a>
+            <a href="/php/index.php#portafolio" class="active">Reseñas</a>
+        </nav>
+    </header>
+
+    <section class="services" id="services">
+
+        <h2 class="heading"><span>Cinammon Hosting</span> Discord Bots</h2>
+
+        <div class="services-container">
+            <div class="services-box">
+                <i class="bx bx-bot"></i>
+                <h3>free plan<span>.cinammon.es</span></h3> <br>
+                <div class="banner-text">
+                    <h1>Alojamiento Discord Bot de alta calidad</h1>
+                    <p>Una nueva aventura te espera con solo unos clics...</p>
+                    <a href="/" class="btn">contrata</a> <br> <br>
+                </div>
+            </div>
+            <div class="services-box">
+                <i class="bx bx-bot"></i>
+                <h3>basic plan<span>.cinammon.es</span></h3> <br>
+                <div class="banner-text">
+                    <h1>Alojamiento Discord Bot de alta calidad</h1>
+                    <p>Una nueva aventura te espera con solo unos clics...</p>
+                    <a href="/index.php" class="btn">contrata</a> <br> <br>
+                </div>
+            </div>
+            <div class="services-box">
+                <i class="bx bx-bot"></i>
+                <h3>premium plan<span>.cinammon.es</span></h3> <br>
+                <div class="banner-text">
+                    <h1>Alojamiento Discord Bot de alta calidad</h1>
+                    <p>Una nueva aventura te espera con solo unos clics...</p>
+                    <a href="/index.php" class="btn">contrata</a> <br> <br>
+                </div>
+            </div>
+
+            <div class="services-box">
+                <i class="bx bx-bot"></i>
+                <h3>ultimate plan<span>.cinammon.es</span></h3> <br>
+                <div class="banner-text">
+                    <h1>Alojamiento Discord Bot de alta calidad</h1>
+                    <p>Una nueva aventura te espera con solo unos clics...</p>
+                    <a href="/index.php" class="btn">contrata</a> <br> <br>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer class="footer">
+
+        <div class="footer-text">
+            <p>&copy; 2023-2030 - Todos los derechos reservados a <a href="#"><span>cinammon.es</span></a></p>
+        </div>
+
+        <div class="footer-iconTop">
+            <a href="#home"><i class="bx bx-up-arrow-alt"></i></a>
+        </div>
+    </footer>
+
+    <script src="/styling/services/scriptdb.js"></script> 
+    <script src="https://unpkg.com/scrollreveal"></script>
+
+</body>
+
+</html>
