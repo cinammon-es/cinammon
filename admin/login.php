13c472a982de1ee4802b7b82797bc38540cf1e8a
lz20r <zhiyaonaiara@gmail.com>
2024-06-02 20:39:59 +0200

    chore: Agregar página de inicio de sesión en el panel de administración

---
+++ admin/login.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio de Sesión</title>
+    <link rel="icon" href="/images/main/favicon.ico">
+    <link rel="stylesheet" href="/styling/admin/login.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="image-container">
+            <img src="/images/main/favicon.ico" alt="Login Image">
+            <p>cinammon.es</p>
+        </div>
+        <div class="login">
+            <h1>Iniciar Sesión</h1>
+            <form id="loginForm">
+                <label for="username" class="sr-only">Nombre de Usuario</label> <br>
+                <input class="form-control" type="text" id="username" name="username" required placeholder="Nombre de Usuario">
+                <input class="form-control" type="password" id="password" name="password" required placeholder="Contraseña">
+                <button class="btn" type="submit" name="login" value="login">Iniciar Sesión</button>
+                <p>¿Olvidaste tu contraseña? <a href="/admin/forgot-password.php">Recupérala</a></p>
+            </form>
+            <p>¿No tienes una cuenta? <a href="/admin/register.php">Regístrate</a></p>
+            <p class="error" style="color:red;"></p>
+        </div>
+    </div>
+    <script src="/styling/admin/login.js"></script>
+</body>
+
+</html>
