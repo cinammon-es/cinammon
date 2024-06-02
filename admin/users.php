60c0d9cc5d1fe6bfcf296d6435fe583eaca906ef
lz20r <zhiyaonaiara@gmail.com>
2024-06-02 21:56:18 +0200

    chore: Actualizar favicon en la página de usuarios de administración

--- admin/users.php
+++ admin/users.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Usuarios Registrados</title>
+
+    <link rel="icon" href="/images/main/favicon.ico">
     <!-- Importar fuentes -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap">
     <style>
