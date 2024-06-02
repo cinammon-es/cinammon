1705cf8c70b55a48936dea394856fe23e09989a4
lz20r <zhiyaonaiara@gmail.com>
2024-06-02 03:09:21 +0200

    chore: Actualizar página de servicios de bots de Discord con nuevos planes y estilos

---
+++ services/discordbotshosting.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>https://cinammon.es/services/discordbot</title>
+
+    <link rel="stylesheet" href="/styling/main/style.css">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
+
+</head>
+
+<body>
+    <header class="header">
+        <a href="https://discord.gg/wRddFGqMdx" class="logo">cinammon.es</a>
+        <i class="bx bx-menu" id="menu-icon" aria-label="Menu icon"></i>
+        <nav aria-label="Main Navigation">
+            <ul class="navbar">
+                <li><a href=" /main/index.php#home" class="active">Inicio</a></li>
+                <li><a href=" /main/index.php#about">Nosotros</a></li>
+                <li><a href=" /main/index.php##account">Cuenta</a></li>
+                <li><a href=" /main/index.php##services">Servicios </a>
+                    <ul class="navbar-dropdown">
+                        <li><a href=" /services/websitehosting.php">Páginas Web</a></li>
+                        <li><a href=" /services/discordbotshosting.php">Bots de Discord</a></li>
+                        <li><a href=" /services/minecrafthosting.php">Minecraft</a></li>
+                        <li><a href=" /services/dedicatedservers.php">Servidores Dedicados</a></li>
+                        <li><a href=" /services/vpshosting.php">VPS</a></li>
+                    </ul>
+                </li>
+                <li><a href=" /main/index.php#billing">Facturación</a></li>
+                <li><a href=" /main/index.php#review">Reseñas</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section class="services" id="services">
+
+        <h2 class="heading"><span>Cinammon Hosting</span> Discord Bots</h2>
+
+        <div class="services-container">
+            <div class="services-box">
+                <i class="bx bx-bot"></i>
+                <h3>free plan<span>.cinammon.es</span></h3> <br>
+                <div class="banner-text">
+                    <h1> </h1>
+                    <p>...</p>
+                    <a href=" /prices/prices.php#discordbot-freeplan" class="btn">contrata</a> <br> <br>
+                </div>
+            </div>
+            <div class="services-box">
+                <i class="bx bx-server"></i>
+                <h3>basic plan<span>.cinammon.es</span></h3> <br>
+                <div class="banner-text">
+                    <h1> </h1>
+                    <p>...</p>
+                    <a href=" /prices/prices.php#discordbot-basicplan" class="btn">contrata</a> <br> <br>
+                </div>
+            </div>
+            <div class="services-box">
+                <i class="bx bx-cloud"></i>
+                <h3>premium plan<span>.cinammon.es</span></h3> <br>
+                <div class="banner-text">
+                    <h1> </h1>
+                    <p>...</p>
+                    <a href=" /prices/prices.php#discordbot-premiumplan" class="btn">contrata</a> <br> <br>
+                </div>
+            </div>
+            <div class="services-box">
+                <i class="bx bx-server"></i>
+                <h3>ultimate plan<span>.cinammon.es</span></h3> <br>
+                <div class="banner-text">
+                    <h1> </h1>
+                    <p>...</p>
+                    <a href=" /prices/prices.php#discordbot-ultimateplan" class="btn">contrata</a> <br> <br>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer class="footer">
+
+        <div class="footer-text">
+            <p>&copy; 2023-2030 - Todos los derechos reservados a <a href="#"><span>cinammon.es</span></a></p>
+        </div>
+
+        <div class="footer-iconTop">
+            <a href="#home"><i class="bx bx-up-arrow-alt"></i></a>
+        </div>
+    </footer>
+
+    <script src="/styling/services/scriptdb.js"></script>
+    <script src="https://unpkg.com/scrollreveal"></script>
+
+</body>
+
+</html>
