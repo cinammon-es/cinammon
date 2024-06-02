ca524898539c0c8ebaea06d7bf5e6e29fc288e3e
lz20r <zhiyaonaiara@gmail.com>
2024-06-02 03:01:07 +0200

    chore: Actualizar página de inicio con nuevos estilos y enlaces

--- php/services/websitehosting.php
+++ php/services/websitehosting.php
@@ -1,102 +1,99 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE-edge"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE-edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>https://cinammon.es/services/website</title>
 
-    <link rel="stylesheet" href="/styling/services/stylewebsite.css"/>
-    <link
-            rel="stylesheet"
-            href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
-    />
+    <link rel="stylesheet" href="/styling/main/style.css" />
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" />
 </head>
+
 <body>
-<header class="header">
-    <a href="/php/index.php#home" class="logo">cinammon.es</a>
+    <header class="header">
+        <a href="https://discord.gg/wRddFGqMdx" class="logo">cinammon.es</a>
+        <i class="bx bx-menu" id="menu-icon" aria-label="Menu icon"></i>
+        <nav aria-label="Main Navigation">
+            <ul class="navbar">
+                <li><a href="/php/main/index.php#home" class="active">Inicio</a></li>
+                <li><a href="/php/main/index.php#about">Nosotros</a></li>
+                <li><a href="/php/main/index.php##account">Cuenta</a></li>
+                <li><a href="/php/main/index.php##services">Servicios </a>
+                    <ul class="navbar-dropdown">
+                        <li><a href="/php/services/websitehosting.php">Páginas Web</a></li>
+                        <li><a href="/php/services/discordbotshosting.php">Bots de Discord</a></li>
+                        <li><a href="/php/services/minecrafthosting.php">Minecraft</a></li>
+                        <li><a href="/php/services/dedicatedservers.php">Servidores Dedicados</a></li>
+                        <li><a href="/php/services/vpshosting.php">VPS</a></li>
+                    </ul>
+                </li>
+                <li><a href="/php/main/index.php#billing">Facturación</a></li>
+                <li><a href="/php/main/index.php#review">Reseñas</a></li>
+            </ul>
+        </nav>
+    </header>
 
-    <i class="bx bx-menu" id="menu-icon"></i>
+    <section class="services" id="services">
+        <h2 class="heading"><span>Cinammon Hosting</span> Websites</h2>
 
-    <nav class="navbar">
-        <a href="/php/index.php#home" class="active">Inicio</a>
-        <a href="/php/index.php#about" class="active">Nosotros</a>
-        <a href="/php/index.php#services" class="active">Servicios</a>
-        <a href="/php/index.php#billing" class="active">Facturación</a>
-        <a href="/php/index.php#portafolio" class="active">Reseñas</a>
-    </nav>
-</header>
-
-<section class="services" id="services">
-    <h2 class="heading"><span>Cinammon Hosting</span> Websites</h2>
-
-    <div class="services-container">
-        <div class="services-box">
-            <i class="bx bx-bot"></i>
-            <h3>free plan<span>.cinammon.es</span></h3>
-            <br/>
-            <div class="banner-text">
-                <h1>Alojamiento VPS de alta calidad</h1>
-                <p>Una nueva aventura te espera con solo unos clics...</p>
-                <a href="/htmls/services/vpshosting.html" class="btn">contrata</a>
-                <br/>
-                <br/>
+        <div class="services-container">
+            <div class="services-box">
+                <i class="bx bx-bot"></i>
+                <h3>free plan<span>.cinammon.es</span></h3> <br>
+                <div class="banner-text">
+                    <h1> </h1>
+                    <p>...</p>
+                    <a href="/php/prices/prices.php#website-freeplan" class="btn">contrata</a> <br> <br>
+                </div>
+            </div>
+            <div class="services-box">
+                <i class="bx bx-server"></i>
+                <h3>basic plan<span>.cinammon.es</span></h3> <br>
+                <div class="banner-text">
+                    <h1> </h1>
+                    <p>...</p>
+                    <a href="/php/prices/prices.php#wesbite-basicplan" class="btn">contrata</a> <br> <br>
+                </div>
+            </div>
+            <div class="services-box">
+                <i class="bx bx-cloud"></i>
+                <h3>premium plan<span>.cinammon.es</span></h3> <br>
+                <div class="banner-text">
+                    <h1> </h1>
+                    <p>...</p>
+                    <a href="/php/prices/prices.php#website-premiumplan" class="btn">contrata</a> <br> <br>
+                </div>
+            </div>
+            <div class="services-box">
+                <i class="bx bx-server"></i>
+                <h3>ultimate plan<span>.cinammon.es</span></h3> <br>
+                <div class="banner-text">
+                    <h1> </h1>
+                    <p>...</p>
+                    <a href="/php/prices/prices.php#website-ultimateplan" class="btn">contrata</a> <br> <br>
+                </div>
             </div>
         </div>
-        <div class="services-box">
-            <i class="bx bx-bot"></i>
-            <h3>basic plan<span>.cinammon.es</span></h3>
-            <br/>
-            <div class="banner-text">
-                <h1>Alojamiento VPS de alta calidad</h1>
-                <p>Una nueva aventura te espera con solo unos clics...</p>
-                <a href="/htmls/services/vpshosting.html" class="btn">contrata</a>
-                <br/>
-                <br/>
-            </div>
+    </section>
+
+    <footer class="footer">
+        <div class="footer-text">
+            <p>
+                &copy; 2023-2030 - Todos los derechos reservados a
+                <a href="#"><span>cinammon.es</span></a>
+            </p>
         </div>
-        <div class="services-box">
-            <i class="bx bx-bot"></i>
-            <h3>premium plan<span>.cinammon.es</span></h3>
-            <br/>
-            <div class="banner-text">
-                <h1>Alojamiento VPS de alta calidad</h1>
-                <p>Una nueva aventura te espera con solo unos clics...</p>
-                <a href="/htmls/services/vpshosting.html" class="btn">contrata</a>
-                <br/>
-                <br/>
-            </div>
+
+        <div class="footer-iconTop">
+            <a href="#home"><i class="bx bx-up-arrow-alt"></i></a>
         </div>
-        <div class="services-box">
-            <i class="bx bx-bot"></i>
-            <h3>enterprise plan<span>.cinammon.es</span></h3>
-            <br/>
-            <div class="banner-text">
-                <h1>Alojamiento VPS de alta calidad</h1>
-                <p>Una nueva aventura te espera con solo unos clics...</p>
-                <a href="/htmls/services/vpshosting.html" class="btn">contrata</a>
-                <br/>
-                <br/>
-            </div>
-        </div>
-    </div>
-</section>
+    </footer>
 
-<footer class="footer">
-    <div class="footer-text">
-        <p>
-            &copy; 2023-2030 - Todos los derechos reservados a
-            <a href="#"><span>cinammon.es</span></a>
-        </p>
-    </div>
+    <script src="/styling/services/scriptvps.js"></script>
 
-    <div class="footer-iconTop">
-        <a href="#home"><i class="bx bx-up-arrow-alt"></i></a>
-    </div>
-</footer>
-
-<script src="/styling/services/scriptvps.js"></script>
-
-<script src="https://unpkg.com/scrollreveal"></script>
+    <script src="https://unpkg.com/scrollreveal"></script>
 </body>
+
 </html>
