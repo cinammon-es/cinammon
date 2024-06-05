7a395fa1f5bbdab4851ce414ed448dc50a6122d8
lz20r <zhiyaonaiara@gmail.com>
2024-06-05 20:57:26 +0200

    feat: Agregar sección de facturación a la página de inicio con funcionalidad de cuenta de usuario

---
+++ includes/index/billing.php
@@ -0,0 +1,14 @@
+<section class="billing" id="billing" aria-labelledby="billing-title">
+    <div class="billing-content">
+        <h3>Bienvenido a</h3>
+        <h1 id="billing-title"><span>billing.cinammon.es</span></h1>
+        <h3>Página web de ventas de <span>Cinammon Host</span></h3>
+        <p>Explora nuestros servicios de hosting, VPS, Minecraft host, y más. Únete a nuestro servidor de Discord para
+            cualquier consulta. Estamos encantados de tenerte aquí, ¡disfruta!</p>
+        <br> <br>
+        <a href="https://billing.cinammon.es" class="btn">Nuestro sistema de Financiación</a>
+    </div>
+    <div class="home-img">
+        <img src="https://i.ibb.co/n8S3DQT/cinammon-logo3.png" alt="Imagen de facturación">
+    </div>
+</section>
