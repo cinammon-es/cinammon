a7d678c44914386b5f8cddda16d5cf23d640b209
lz20r <zhiyaonaiara@gmail.com>
2024-06-05 20:57:34 +0200

    chore: Add home section to index page with welcome message and Discord link

---
+++ includes/index/home.php
@@ -0,0 +1,15 @@
+<section class="home" id="home" aria-labelledby="home-title">
+        <div class="home-content">
+            <h3>Bienvenido a</h3>
+            <h1 id="home-title"><span>cinammon.es</span></h1>
+            <h3>Página web de <span>Cinammon Host</span></h3>
+            <p>¡Bienvenido a cinammon.es! Explora nuestros servicios de hosting, VPS, Minecraft host, etc. Recuerda que
+                tienes el servidor de Discord donde puedes hacer todo tipo de preguntas y dudas. Estamos encantados de
+                tenerte en nuestra página web, ¡disfruta!</p>
+            <br> <br>
+            <a href="https://discord.gg/wRddFGqMdx" class="btn">Servidor de Discord</a>
+        </div>
+        <div class="home-img">
+            <img src="https://i.ibb.co/5GFjt3x/cinammon-logo.png" alt="Imagen de bienvenida">
+        </div>
+    </section>
