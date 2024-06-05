6c8fe4209fde01023ff9b6758a597443df2106b9
lz20r <zhiyaonaiara@gmail.com>
2024-06-05 20:57:38 +0200

    chore: Update index page with navigation bar and links

---
+++ includes/index/nav.php
@@ -0,0 +1,22 @@
+<header class="header">
+        <a href="https://discord.gg/wRddFGqMdx" class="logo">cinammon.es</a>
+        <i class="bx bx-menu" id="menu-icon" aria-label="Menu icon"></i>
+        <nav aria-label="Main Navigation">
+            <ul class="navbar">
+                <li><a href="#home" class="active">Inicio</a></li>
+                <li><a href="#about">Nosotros</a></li>
+                <li><a href="#account">Cuenta</a></li>
+                <li><a href="#services">Servicios </a>
+                    <ul class="navbar-dropdown">
+                        <li><a href="/services/websitehosting.php">Páginas Web</a></li>
+                        <li><a href="/services/discordbotshosting.php">Bots de Discord</a></li>
+                        <li><a href="/services/minecrafthosting.php">Minecraft</a></li>
+                        <li><a href="/services/dedicatedservers.php">Servidores Dedicados</a></li>
+                        <li><a href="/services/vpshosting.php">VPS</a></li>
+                    </ul>
+                </li>
+                <li><a href="#billing">Facturación</a></li>
+                <li><a href="#review">Reseñas</a></li>
+            </ul>
+        </nav>
+    </header>
