fd66a206eea7d84bd979edb4ca3154c4d4020ea9
lz20r <zhiyaonaiara@gmail.com>
2024-06-05 20:57:18 +0200

    chore: Add about section to index page with information about the company

---
+++ includes/index/about.php
@@ -0,0 +1,12 @@
+<section class="about" id="about" aria-labelledby="about-title">
+        <div class="about-img">
+            <img src="https://i.ibb.co/fd9bqQD/cinammon-logo.png" alt="Sobre nosotros">
+        </div>
+        <div class="about-content">
+            <h2 class="heading" id="about-title">¿Por qué <span>Escogernos</span>?</h2>
+            <p>Ofrecemos un servicio de hosting completo, rápido e interactivo. Garantizamos continuidad sin cortes y
+                contamos con personal disponible 24/7 para resolver cualquier problema. Disfruta de una experiencia de
+                hosting fiable y eficiente con soporte dedicado en todo momento.</p>
+            <a href="https://discord.gg/uRCTQZdgUh" class="btn">Más información</a>
+        </div>
+    </section>
