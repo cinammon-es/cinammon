3fe222cd8cda638a6a801c179fb593de72f63da7
lz20r <zhiyaonaiara@gmail.com>
2024-06-05 20:55:37 +0200

    chore: Update index.php with includes for navigation, home, about, account, services, billing, review, and footer

---
+++ index.php
@@ -0,0 +1,37 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cinammon.es</title>
+    <meta name="description" content="Explora nuestros servicios de hosting, VPS, Minecraft host, y más en cinammon.es. Únete a nuestro servidor de Discord para cualquier consulta.">
+    <meta name="keywords" content="hosting, VPS, Minecraft host, servicios, Discord, cinammon">
+    <meta name="author" content="cinammon.es">
+
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="icon" href="/images/main/favicon.ico">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
+</head>
+
+<body> 
+
+    <?php include 'includes/index/nav.php'; ?>
+
+    <?php include 'includes/index/home.php'; ?>
+
+    <?php include 'includes/index/about.php'; ?>
+
+    <?php include 'includes/index/account.php'; ?>
+
+    <?php include 'includes/index/services.php'; ?> 
+
+    <?php include 'includes/index/billing.php'; ?>
+    
+    <?php include 'includes/index/review.php'; ?>
+
+    <?php include 'includes/index/footer.php'; ?>
+    
+    <script src="assets/js/script.js"></script>
+    <script src="https://unpkg.com/scrollreveal"></script>
+</body>
+
+</html>
