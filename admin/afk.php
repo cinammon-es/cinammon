5fd95b04e483d665177d22c9bf47a4369374cb2a
lz20r <zhiyaonaiara@gmail.com>
2024-06-05 21:06:57 +0200

    chore: Add favicon to AFK Manager page

--- admin/afk.php
+++ admin/afk.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AFK Manager</title>
+    <link rel="icon" href="/images/main/favicon.ico">
     <link rel="stylesheet" href="/assets/css/afk.css">
 </head>
 
