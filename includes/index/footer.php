fb59b56c6e94ea0a94f6b5ca22bddac83933a7e6
lz20r <zhiyaonaiara@gmail.com>
2024-06-05 20:57:30 +0200

    chore: Add footer to index page with copyright information and scroll reveal script

---
+++ includes/index/footer.php
@@ -0,0 +1,13 @@
+<footer class="footer" aria-label="Footer">
+    <div class="footer-text">
+        <p>&copy; 2022 Cinammon. All Rights Reserved.</p>
+    </div>
+    <div class="footer-iconTop">
+        <a href="#home"><i class="bx bx-up-arrow-alt"></i></a>
+    </div>
+</footer>
+
+<script src="/assets/js/script.js"></script>
+<script src="https://unpkg.com/scrollreveal"></script>
+
+</body>
