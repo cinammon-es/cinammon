e2bff92dfeab38d20e89af811aa870b90a96e380
lz20r <zhiyaonaiara@gmail.com>
2024-06-05 20:57:22 +0200

    chore: Add account section to index page with user account functionality

---
+++ includes/index/account.php
@@ -0,0 +1,32 @@
+<section id="account" aria-labelledby="account-title">
+        <div class="account">
+            <h2 class="heading" id="account-title">¿Qué puedes hacer en tu <span>Cuenta</span>?</h2>
+            <div class="account-container">
+                <div class="account-box">
+                    <i class="bx bx-user"></i>
+                    <h3>Perfil</h3>
+                    <p>En tu perfil podrás ver toda la información de tu cuenta, como tu correo, tu nombre, tu ID, y
+                        más.</p>
+                    <a href="/admin/register.php" class="btn">Ver más</a>
+                </div>
+                <div class="account-box">
+                    <i class="bx bx-credit-card"></i>
+                    <h3>Facturación</h3>
+                    <p>En la sección de facturación podrás ver todas tus facturas y descargarlas en PDF.</p>
+                    <a href=" /account/billing.html" class="btn">Ver más</a>
+                </div>
+                <div class="account-box">
+                    <i class="bx bx-server"></i>
+                    <h3>Servicios</h3>
+                    <p>En servicios podrás ver todos los servicios que tienes contratados y la información de cada uno.</p>
+                    <a href=" /account/services.html" class="btn">Ver más</a>
+                </div>
+                <div class="account-box">
+                    <i class="bx bx-support"></i>
+                    <h3>Soporte</h3>
+                    <p>En soporte podrás ver todas las incidencias que has tenido y el estado de cada una.</p>
+                    <a href=" /account/support.html" class="btn">Ver más</a>
+                </div>
+            </div>
+        </div>
+    </section>
